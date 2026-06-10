78701c81eeecbaab1199643616089eaa0c45b0ca
Jordan Pavez <jordanmarzo2001@gmail.com>
2026-06-10 19:24:45 -0400

    Mis primeros scripts de lógica en Python

---
+++ practica.py
@@ -0,0 +1,23 @@
+def usar_pocion(vida_actual, esta_envenenado):
+    pocion = 50
+    
+    if esta_envenenado:
+        pocion = 25
+        print("⚠️ ¡Estás envenenado! La poción cura la mitad.")
+    
+    vida_final = vida_actual + pocion
+    
+    if vida_final > 100:
+        vida_final = 100
+        print("❤️ ¡Vida al máximo!")
+    else:
+        print(f"✨ Te has curado. Vida actual: {vida_final}")
+        
+    return vida_final
+
+# --- Probando el script ---
+print("--- Simulando inicio de partida ---")
+vida_jugador = 60
+
+# Probamos usando la poción estallando el veneno en True
+vida_jugador = usar_pocion(vida_jugador, esta_envenenado=True)
